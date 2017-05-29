3fc26f98aad99f30ece4ce04b2198c48be381b77
Evan Hoover <ehoover4567@gmail.com>
2017-05-29 03:25:58 -0500

    Update are_two_questions_the_same.py

--- quora_question_matching_nlp/are_two_questions_the_same.py
+++ quora_question_matching_nlp/are_two_questions_the_same.py
@@ -1,1 +1,51 @@
+# given two quora question strings, identify which questions are essentially asking the same thing using different words
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import gc
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+pal = sns.color_palette()
+os.chdir(r'D:\DSHub\kaggle\QuoraNLPQuestions')
+
+# # looking into the datasets
+
+df_train = pd.read_csv(r'./input/train.csv')
+df_train.head()
+
+# # NOTES
+'''is_duplicate = the label we are going to try and predict....this denotates whether a similarly worded question is
+    actually the same question.'''
+    
+print('Total number of question pairs for training: {}'.format(len(df_train)))
+
+print('Duplicate pairs: {}%'.format(round(df_train['is_duplicate'].mean()*100, 2)))
+
+qids = pd.Series(df_train['qid1'].tolist() + df_train['qid2'].tolist())
+
+print('Total number of questions in the training data: {}'.format(len(
+    np.unique(qids))))
+
+print('Number of questions that appear multiple times: {}'.format(np.sum(qids.value_counts() > 1)))
+
+plt.figure(figsize=(12, 5))
+plt.hist(qids.value_counts(), bins=50)
+plt.yscale('log', nonposy='clip')
+plt.title('Log-Histogram of question appearance counts')
+plt.xlabel('Number of occurences of question')
+plt.ylabel('Number of questions')
+print()
+
+from sklearn.metrics import log_loss
+
+p = df_train['is_duplicate'].mean() # Our predicted probability
+print('Predicted score:', log_loss(df_train['is_duplicate'], np.zeros_like(df_train['is_duplicate']) + p))
+
+df_test = pd.read_csv('./input/test.csv')
+sub = pd.DataFrame({'test_id': df_test['test_id'], 'is_duplicate': p})
+sub.to_csv('naive_submission.csv', index=False)
+sub.head()
 
